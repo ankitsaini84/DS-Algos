a77fe2ce19e50ae18304129c83278a1c06af7a1e
Ankit Saini <ankit_saini@hotmail.com>
2020-12-24 22:17:37 +0530

    remove vowels from a string

---
+++ Easy/Remove Vowels From A String.cpp
@@ -0,0 +1,37 @@
+/**
+ * Given a string s, remove the vowels 'a', 'e', 'i', 'o', and 'u' from it,
+ * and return the new string.
+ * Example 1:
+ * Input: s = "codingisfunandfuniscoding"
+ * Output: "cdngsfnndfnscdng"
+ * Example 2:
+ * Input: s = "aeiou"
+ * Output: "" 
+ */
+#include <iostream>
+#include <string>
+
+class Solution {
+    public:
+        std::string removeVowels(std::string s) {
+            // Just iterate over the string & keep adding the alphabet to
+            // resultant string - if it is NOT a vowel.
+            int length = s.length();
+            int j {0};
+            for(int i {0}; i < length; ++i) {
+                if(s[i] != 'a' && s[i] != 'e' && s[i] != 'i' && s[i] != 'o' && s[i] != 'u') {
+                    s[j++] = s[i];
+                }
+            }
+            s.resize(j);
+            return s;
+        }
+};
+
+int main() {
+    Solution s;
+    std::string str {"codingisfunandfuniscoding"};
+    std::string result = s.removeVowels(str);
+    std::cout << result;
+    return 0;
+}
