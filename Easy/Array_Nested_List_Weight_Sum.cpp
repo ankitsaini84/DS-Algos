f730dcb535fb2a066330b2f35bd63ad67b6bb4ae
Ankit Saini <ankit_saini@hotmail.com>
2021-01-16 20:10:55 +0530

    Nested List Weight Sum

--- Easy/Array_Nested_List_Weight_Sum.cpp
+++ Easy/Array_Nested_List_Weight_Sum.cpp
@@ -3,16 +3,16 @@
  * Each element is either an integer or a list whose elements may also be integers or other lists.
  * 
  * The depth of an integer is the number of lists that it is inside of. 
- * EXAMPLE, the nested list [1,[2,2],[[3],2],1] has each integer's value set to its depth.
+ * For example, the nested list [1,[2,2],[[3],2],1] has each integer's value set to its depth.
  * 
  * Return the sum of each integer in nestedList multiplied by its depth.
  * 
  * EXAMPLE:
  * Input: nestedList = [[1,1],2,[1,1]]
  * Output: 10
- * Explanation: Four 1's at depth 2, one 2 at depth 1. 1*2 + 1*2 + 2*1 + 1*2 + 1*2 = 10.
+ * EXPLAINATION: Four 1's at depth 2, one 2 at depth 1. 1*2 + 1*2 + 2*1 + 1*2 + 1*2 = 10.
  * 
- * Constraints:
+ * CONSTRAINTS:
  * 1 <= nestedList.length <= 50
  * The values of the integers in the nested list is in the range [-100, 100].
  * The maximum depth of any integer is less than or equal to 50.
