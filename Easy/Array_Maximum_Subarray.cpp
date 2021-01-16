134816cc5ead4efc0375ab55e67dd77d000524b4
Ankit Saini <ankit_saini@hotmail.com>
2021-01-16 20:09:30 +0530

    Maximum Subarray

--- Easy/Array_Maximum_Subarray.cpp
+++ Easy/Array_Maximum_Subarray.cpp
@@ -2,10 +2,11 @@
  * Given an integer array nums, find the contiguous subarray 
  * (containing at least one number) which has the largest sum and return 
  * its sum.
- * EXAMPLE 1:
+ * EXAMPLE:
  * Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
  * Output: 6
- * Explanation: [4,-1,2,1] has the largest sum = 6.
+ * 
+ * NOTE: [4,-1,2,1] has the largest sum = 6.
  */
 
 #include <iostream>
