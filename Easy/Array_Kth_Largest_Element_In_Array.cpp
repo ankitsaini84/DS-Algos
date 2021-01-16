020533fb3558e00493562fba1dff28c463a8a66a
Ankit Saini <ankit_saini@hotmail.com>
2021-01-16 20:14:28 +0530

    Kth Largest Element In Array

--- Easy/Array_Kth_Largest_Element_In_Array.cpp
+++ Easy/Array_Kth_Largest_Element_In_Array.cpp
@@ -2,11 +2,11 @@
  * Find the kth largest element in an unsorted array. 
  * Note that it is the kth largest element in the sorted order, not the kth distinct element.
  * 
- * EXAMPLE 1:
+ * EXAMPLE:
  * Input: [3,2,1,5,6,4] and k = 2
  * Output: 5
  * 
- * EXAMPLE 2:
+ * EXAMPLE:
  * Input: [3,2,3,1,2,4,5,5,6] and k = 4
  * Output: 4
  * 
