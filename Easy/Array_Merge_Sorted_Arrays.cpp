8c593c15c660dafbbe30f054eb8766129fb4c8b9
Ankit Saini <ankit_saini@hotmail.com>
2021-01-16 22:27:57 +0530

    Merge Sorted Array

--- Easy/Array_Merge_Sorted_Arrays.cpp
+++ Easy/Array_Merge_Sorted_Arrays.cpp
@@ -18,7 +18,40 @@
 class Solution {
 public:
     void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n) {
-        std::vector
-        for(auto i = nums1.begin(); i != nums1.end(); ++i)
+        int x {m - 1};
+        int y {n - 1};
+        int z {m + n - 1};
+
+        while(x >= 0 && y >= 0) {
+            if(nums1[x] > nums2[y]) {
+                nums1[z] = nums1[x];
+                --x;
+            } else {
+                nums1[z] = nums2[y];
+                --y;
+            }
+            --z;
+        }
+
+        while(y >= 0) {
+            nums1[z] = nums2[y];
+            --y;
+            --z;
+        }
     }
 };
+
+int main() {
+    std::vector<int> nums1 {4, 5, 6, 0, 0, 0};
+    std::vector<int> nums2 {1, 2, 3};
+
+    Solution s;
+    s.merge(nums1, 3, nums2, 3);
+    std::cout << "Solution : ";
+    for(const int& i : nums1) {
+        std::cout << i << ' ';
+    }
+    std::cout << '\n';
+
+    return 0;
+}
